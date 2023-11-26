09082c0550c46e86a05ec59ab2046a6a0e9ec23f
akasatana137 <tatsuya1412kusama@gmail.com>
2023-11-26 16:00:10 +0900

    fix buildspec.yml

--- backend/deploy/set_env_variables.sh
+++ backend/deploy/set_env_variables.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo DB_HOST=$DB_HOST >> laravel/.env &&
-echo DB_DATABASE=$DB_DATABASE >> laravel/.env &&
-echo DB_USERNAME=$DB_USERNAME >> laravel/.env &&
-echo DB_PASSWORD=$DB_PASSWORD >> laravel/.env &&
-echo APP_NAME=$APP_NAME >> laravel/.env &&
-echo APP_KEY=$APP_KEY >> laravel/.env &&
-echo APP_ENV=$APP_ENV >> laravel/.env &&
-echo APP_DEBUG=$APP_DEBUG >> laravel/.env &&
+echo DB_HOST=$DB_HOST >> backend/.env &&
+echo DB_DATABASE=$DB_DATABASE >> backend/.env &&
+echo DB_USERNAME=$DB_USERNAME >> backend/.env &&
+echo DB_PASSWORD=$DB_PASSWORD >> backend/.env &&
+echo APP_NAME=$APP_NAME >> backend/.env &&
+echo APP_KEY=$APP_KEY >> backend/.env &&
+echo APP_ENV=$APP_ENV >> backend/.env &&
+echo APP_DEBUG=$APP_DEBUG >> backend/.env &&
 echo 'Laravel env variables configured'
